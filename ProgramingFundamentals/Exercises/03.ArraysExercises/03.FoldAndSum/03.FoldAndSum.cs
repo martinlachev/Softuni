1675ed5efbca6b1ba63eca1bd1ae209e60578a62
Martin Lachev <martinlachev1@gmail.com>
2016-05-30 15:02:14 +0300

    ProgramingFundamentalsUpdate3.0

--- ProgramingFundamentals/Exercises/03.ArraysExercises/03.FoldAndSum/03.FoldAndSum.cs
+++ ProgramingFundamentals/Exercises/03.ArraysExercises/03.FoldAndSum/03.FoldAndSum.cs
@@ -14,12 +14,52 @@ namespace _03.FoldAndSum
                 .Select(int.Parse).ToArray();
 
             int k = arr.Length / 2;
+            int splitK = k / 2;
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-               
-            }
             
+            List<int> middle = new List<int>();
+            List<int> outsideFirst = new List<int>();
+            List<int> outsideSecond = new List<int>();
+            List<int> sumOutside = new List<int>();
+
+            for (int i = 0 ; i < arr.Length; i++)
+            {
+                if (i>=k-splitK && i<=k+splitK-1)
+                {
+                    middle.Add(arr[i]);
+                }
+                else if(i >= 0 && i <= splitK)
+                {
+                    outsideFirst.Add(arr[i]);
+                }
+                else
+                {
+                    outsideSecond.Add(arr[i]);
+                }
+            }
+
+            outsideFirst.Reverse();
+            outsideSecond.Reverse();
+
+            foreach (var item in outsideFirst)
+            {
+                sumOutside.Add(item);
+
+            }
+
+            foreach (var item in outsideSecond)
+            {
+                sumOutside.Add(item);
+            }
+
+            var sum = middle.Zip(sumOutside, (x, y) => x + y).ToList();
+
+           
+
+            foreach (var item in sum)
+            {
+                Console.Write(item+" ");
+            }
         }
 
     }
