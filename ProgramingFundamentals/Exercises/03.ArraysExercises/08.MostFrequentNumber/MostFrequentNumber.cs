0666353204f139c33970df749f0120819f26d5ba
Martin Lachev <martinlachev1@gmail.com>
2016-06-10 22:01:33 +0300

    Update 5.0

---
+++ ProgramingFundamentals/Exercises/03.ArraysExercises/08.MostFrequentNumber/MostFrequentNumber.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.MostFrequentNumber
+{
+    class MostFrequentNumber
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Array.Sort(numbers);
+
+            Console.WriteLine(get_occure(numbers));
+            
+
+        }
+        public static int get_occure(int[] a)
+        {
+            int[] arr = a;
+            int c = 1, maxcount = 1, maxvalue = 0;
+            int result = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                maxvalue = arr[i];
+                for (int j = 0; j < arr.Length; j++)
+                {
+
+                    if (maxvalue == arr[j] && j != i)
+                    {
+                        c++;
+                        if (c > maxcount)
+                        {
+                            maxcount = c;
+                            result = arr[i];
+
+                        }
+                    }
+                    else
+                    {
+                        c = 1;
+
+                    }
+
+                }
+
+
+            }
+            return result;
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/03.ArraysExercises/10.Pairs/Pairs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Pairs
+{
+    class Pairs
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int diff = int.Parse(Console.ReadLine());
+
+            int pairs = 0;
+            for (int i = 0; i < input.Length-1; i++)
+            {
+                for (int j = i+1; j < input.Length; j++)
+                {
+                    if (Math.Abs(input[i] - input[j]) == diff)
+                    {
+                        pairs++;
+                    }
+                    
+                }
+            }
+            Console.WriteLine(pairs);
+        }
+    }
+}
