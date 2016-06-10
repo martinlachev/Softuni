6b832d6c95565ce93b85646813257fe89c1ac84f
Martin Lachev <martinlachev1@gmail.com>
2016-06-10 23:40:22 +0300

    Update5.0

---
+++ ProgramingFundamentals/Exercises/04.ExerciseListsAndMatrices/01.MaxSequenceOfEqualElements/MaxSequenceOfEqualElements.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.MaxSequenceOfEqualElements
+{
+    class MaxSequenceOfEqualElements
+    {
+        static void Main(string[] args)
+        {
+            var array1 = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int count_max = 0;
+            int start_max = 0;
+            int count = 0;
+            for (int i = 0; i < array1.Length - 1; i++)
+            {
+                count = 0;
+                while (array1[i + count] == array1[i + 1 + count])
+                {
+                    count++;
+                    if (i + 1 + count > array1.Length - 1)
+                    {
+                        break;
+                    }
+
+                }
+                if (count > count_max)
+                {
+                    count_max = count;
+                    start_max = i;
+                }
+
+            }
+            for (int i = start_max; i <= start_max + count_max; i++)
+            {
+                Console.Write($"{array1[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ProgramingFundamentals/Exercises/04.ExerciseListsAndMatrices/06.HourglassSum/HourglassSum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.HourglassSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
