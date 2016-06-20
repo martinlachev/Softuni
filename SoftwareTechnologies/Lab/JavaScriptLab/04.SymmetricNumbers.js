4bf73fdacf2f57516ab26a9c67787051e1c75bab
Martin Lachev <martinlachev1@gmail.com>
2016-06-20 17:50:56 +0300

    Update6.0

--- SoftwareTechnologies/Lab/JavaScriptLab/04.SymmetricNumbers.js
+++ SoftwareTechnologies/Lab/JavaScriptLab/04.SymmetricNumbers.js
@@ -1,7 +1,26 @@
 /**
  * Created by MartinLachev on 6/20/2016.
  */
-function isSymetric(arr){
-    
 
+function symetricNumbers(arr) {
+    let n = Number(arr[0]),result = ''
+    for (var i = 0; i < n; i++) {
+        if(isSymetric(""+i)){
+            result += i + " "
+        }
+        console.log(result)
+
+    }
+    function isSymetric(str){
+        for (var i = 0; i < str.length; i++) {
+            if(str[i] != str[str.length - i - 1]){
+                return false
+            }
+            else {
+                return true
+            }
+        }
+
+    }
 }
+symetricNumbers(['1000'])
